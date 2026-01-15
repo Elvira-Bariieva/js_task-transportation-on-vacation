8ba6917f9a9d272c8f60ce3b23c84c343ba04b5f
Elvira Bariieva <160289070+Elvira-Bariieva@users.noreply.github.com>
2026-01-15 18:31:17 -0400

    Update calculateRentalCost.js

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_PER_DAY = 40;
+  const SHORT_TERM = 3;
+  const LONG_TERM = 7;
+  const SHORT_TERM_DISCOUNT = 20;
+  const LONG_TERM_DISCOUNT = 50;
+
+  const baseTotalPrice = PRICE_PER_DAY * days;
+
+  if (days < SHORT_TERM) {
+    return baseTotalPrice;
+  }
+
+  if (days < LONG_TERM) {
+    return baseTotalPrice - SHORT_TERM_DISCOUNT;
+  }
+
+  return baseTotalPrice - LONG_TERM_DISCOUNT;
 }
 
 module.exports = calculateRentalCost;
